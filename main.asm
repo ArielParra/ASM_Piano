a48f9b821490d4a4ccfe38a0f6c608c95ef4f1fd
Ariel Parra <arielemilio_parra@hotmail.com>
2024-04-17 00:15:33 -0600

    todo binary vec to hex to use DD

--- main.asm
+++ main.asm
@@ -1,5 +1,13 @@
-;
-;https://github.com/zserge/beep
+
+usleep MACRO 
+         
+         MOV DX, word ptr [microseconds]     ; baja
+         MOV CX, word ptr [microseconds + 2] ; alta
+ 
+         XOR   AL,AL
+         MOV   AH, 86H
+         INT   15H
+ENDM
 .model small
 
 .stack 100h
@@ -12,7 +20,9 @@ int16_var1 DW 0
 int8_var2 DB 0
 str_string1 DB "hola mundo",'$';
 str_string2 DB "preciona a",'$';
-
+microseconds DD 500000;500ms
+;TODO binary vec
+;TODO binary to hex
 
 ; Define frequencies
 C       equ 4560    ; Middle C
@@ -55,103 +65,26 @@ main PROC
         
     JMP while1   
     eliwh1:
-  
-        ; Play "Twinkle, Twinkle, Little Star"
+        ; Play "Twinkle, Twinkle, Little Star
         putch  'C'
-        call    PlayC
-        CALL DelayBetweenNotes
-        putch  'C'
-        call    PlayC
-        putch 'G'
-        call    PlayG
-                CALL DelayBetweenNotes
-        putch 'G'
-        call    PlayG
-        putch 'A'
-        call    PlayA
-                CALL DelayBetweenNotes
-
-        putch 'A'
-        call    PlayA
-        putch 'G'
-        call    PlayG
-        putch 'F'
-        call    PlayF
-                CALL DelayBetweenNotes
-
-        putch 'F'
-        call    PlayF
-        putch 'E'
-        call    PlayE
-                CALL DelayBetweenNotes
-
-        putch 'E'
-        call    PlayE
-        putch 'D'
-        call    PlayD
-                CALL DelayBetweenNotes
-
-        putch 'D'
-        call    PlayD
-        putch  'C'
-        call    PlayC
- 
- 
-
-    
-exit
-main ENDP
-
-    ; Procedures to play each note
-    PlayC proc
         mov     ax, C
         mov     bx, Medium
         call    Beep
-        ret
-    PlayC endp
-
-    PlayG proc
-        mov     ax, G
+        
+        usleep 
+        putch  'C'
+        mov     ax, C
         mov     bx, Medium
         call    Beep
-        ret
-    PlayG endp
-
-    PlayA proc
-        mov     ax, A
+        usleep 
+            putch  'C'
+        mov     ax, C
         mov     bx, Medium
         call    Beep
-        ret
-    PlayA endp
 
-    PlayF proc
-        mov     ax, F
-        mov     bx, Medium
-        call    Beep
-        ret
-    PlayF endp
-
-    PlayE proc
-        mov     ax, E
-        mov     bx, Medium
-        call    Beep
-        ret
-    PlayE endp
-
-    PlayD proc
-        mov     ax, D
-        mov     bx, Medium
-        call    Beep
-        ret
-    PlayD endp
-
-DelayBetweenNotes PROC
-    mov     cx, 65535         ; Adjust this value to control the delay
-.delay_loop:
-    dec     cx
-    jnz     .delay_loop
-    ret
-DelayBetweenNotes ENDP
+ 
+exit
+main ENDP
 
 
 
@@ -189,5 +122,3 @@ Beep ENDP
 
 END;code segment
 
-;https://www.cs.binghamton.edu/~reckert/220/8254_timer.html
-;https://web.archive.org/web/20160712192529/http://muruganad.com/8086/8086-assem
