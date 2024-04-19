819470232cf21b1349f04494d6c5557896f75a40
Ariel Parra <arielemilio_parra@hotmail.com>
2024-04-19 13:29:27 -0600

    ya jalando

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Proyecto_Ensamblador
 proyecto de un piano en ensamblador 8086 para DOS
-hola mundo
-hola diego
--- main.asm
+++ main.asm
@@ -9,10 +9,12 @@ buff_input DB 10,?,10 DUP(?)
 int16_aux DW 0
 int8_var2 DB 0
 str_string1 DB "hola mundo",'$';
-str_string2 DB "preciona a",'$';
+str_string2 DB "preciona 1,2,3",'$';
 
 
-; constants notes
+jump_table DW case_1, case_2, case_3, case_default;
+
+
 note_C       equ 4560    ; Middle C
 note_G       equ 6087    ; G
 note_A       equ 5423    ; A
@@ -40,25 +42,47 @@ main PROC
         kbhit 
         JZ while1
           
-        getch
-        CMP AL,'a' 
-        JE eliwh1  
-       
+         getch
+         XOR AH,AH
+         SUB AX,30h;quitarle char 
+         DEC AX;para empezar en 0
+         
+         
+         ;validar rango
+         CMP AX,3; pq solo son 3 de 0 a 2
+         JL et_validado
+         MOV AX,3;default
+         
+         et_validado:
+         SHL AX,1
+         LEA BX,jump_table
+         ADD BX,AX;indice ya validado
+         JMP [BX]
+         
+         case_1:
+             putch 'A'
+             beep note_A,1000
+             JMP break
+         
+         case_2:
+             putch 'F'
+             beep note_F,1000
+             JMP break
+         
+         case_3:
+             putch 'C'
+            beep note_C,1000
+             JMP break
+         
+         case_default:
+             putch 'd'
+             JMP eliwh1;salir con def
+         break:
+         
         
     JMP while1   
     eliwh1:
-        ; Play "Twinkle, Twinkle, Little Star
-        putch  'C'
-        beep note_C, 1000
 
-        sleep 1000
-        putch  'C'
-
-        beep note_C, 1000
-        sleep 255
-
-        putch  'C'
-        beep note_C, 1000
 
         
 
